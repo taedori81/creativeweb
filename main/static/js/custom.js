8e1f0b9f5509b53ebe13a5e0a08f8a7201f558f0
taedori <taedori@outlook.com>
2015-04-28 10:16:36 -0700

    Skills section finished

--- main/static/js/custom.js
+++ main/static/js/custom.js
@@ -47,6 +47,32 @@
 		});
 
 */
+    
+        /* ---------------------------------------------- /*
+		 * Quote Rotator
+		/* ---------------------------------------------- */
+       
+			$( function() {
+				/*
+				- how to call the plugin:
+				$( selector ).cbpQTRotator( [options] );
+				- options:
+				{
+					// default transition speed (ms)
+					speed : 700,
+					// default transition easing
+					easing : 'ease',
+					// rotator interval (ms)
+					interval : 8000
+				}
+				- destroy:
+				$( selector ).cbpQTRotator( 'destroy' );
+				*/
+
+				$( '#cbp-qtrotator' ).cbpQTRotator();
+
+			} );
+		
 
 
 		/* ---------------------------------------------- /*
